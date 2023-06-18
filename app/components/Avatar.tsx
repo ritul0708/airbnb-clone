d10a980ad94bc8a8438b39d50e981c8ebf6f0460
Ritul Jain <rituljain0708@gmail.com>
2023-06-19 02:11:12 +0530

    navbar outline done

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Avatar = () => {
+  return (
+    <Image 
+      src='/images/placeholder.jpg'
+      alt='Avatar'
+      width='30'
+      height='30'
+      className='rounded-full'
+    />
+  )
+}
+
+export default Avatar
---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const Logo = () => {
+  const router = useRouter();
+
+  return (
+    <Image
+      src='/images/logo.png'
+      alt='logo'
+      className='hidden md:block cursor-pointer'
+      width={100}
+      height={100}
+    />
+  )
+}
+
+export default Logo
