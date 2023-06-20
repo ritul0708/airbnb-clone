a0993e5e0bbfe3d7f0c7163e845355a8cc8c0ae2
Ritul Jain <rituljain0708@gmail.com>
2023-06-21 02:24:50 +0530

    added logout functionality also fixed some type warnings

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,10 +6,11 @@ import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { User } from '@prisma/client';
+import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
 
 interface UserMenuProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({
@@ -72,7 +73,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                 />
                 <hr />
                 <MenuItem 
-                  onClick={() => {}}
+                  onClick={() => signOut()}
                   label='Logout'
                 />
               </>
