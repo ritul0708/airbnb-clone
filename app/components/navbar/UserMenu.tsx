50e4aeff56582b0a785e435f7d64b5c6c28203ae
Ritul Jain <rituljain0708@gmail.com>
2023-06-24 20:08:27 +0530

    show favorite by User - [ ] Add a new page that shows all favorites of current user

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -67,7 +67,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   label='My Trips'
                 />
                 <MenuItem 
-                  onClick={() => {}}
+                  onClick={() =>router.push('/favorites')}
                   label='My Favorites'
                 />
                 <MenuItem 
